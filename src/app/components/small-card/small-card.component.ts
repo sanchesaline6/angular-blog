6298fb7a132d39f5fa467ea0e0920045d4a0a140
aeoh <aline.sanches@gft.com>
2022-09-23 18:53:42 -0300

    updates link paths

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -7,6 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
   @Input()
+  id: string = '0';
+  @Input()
   photoCover: string = '';
   @Input()
   photoTitle: string = '';
